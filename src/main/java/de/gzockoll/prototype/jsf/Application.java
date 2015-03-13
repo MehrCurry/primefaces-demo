2c0a3f8a573d2eb746e34a788d07941bf81c3b59
Guido Zockoll <gzockoll@gmail.com>
2015-03-13 21:51:37 +0100

    Initial Import

---
+++ src/main/java/de/gzockoll/prototype/jsf/Application.java
@@ -0,0 +1,43 @@
+package de.gzockoll.prototype.jsf;
+
+import com.sun.faces.config.ConfigureListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application implements ServletContextAware {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new FacesServlet(), "*.xhtml");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(
+                new ConfigureListener());
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+    }
+}
